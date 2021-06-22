4b8afa4338c1548cbbdf7eec2cc0981046e277b6
Jonathan Quintero Giraldo <jonathan.quintero@newshore.es>
2021-06-21 19:25:29 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);    
+    this.state = { text: '' }
+  } 
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" onChange={ (event) =>  this.setState({ text: event.target.value })} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.text}</p>
       </div>
     );
   }
